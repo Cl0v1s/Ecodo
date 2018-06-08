db88243240693e809f02dfb696cb41ca847240d3
Administrateur <Administrateur@VS2017>
2018-06-08 10:07:07 +0200

    Finalisation historique

--- Fuite/FuiteAdmin/History.aspx
+++ Fuite/FuiteAdmin/History.aspx
@@ -2,4 +2,20 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Button runat="server" ID="new" OnClick="new_Click" Text="Nouvelle" />
+    <asp:Button runat="server" ID="current" OnClick="current_Click" Text="En cours" />
+    <asp:Button runat="server" ID="closed" OnClick="closed_Click" Text="Fermé" />
+    <table>
+        <thead>
+            <tr>
+                <td>ID</td><td>Statut</td><td>Date</td><td>Action</td>
+            </tr>
+        </thead>
+        <tbody id="history" runat="server">
+
+        </tbody>
+    </table>
+    <div>
+        Pour rechercher un signalement, entrez le raccourcis Ctrl+F et entrez son identifiant dans le champ de recherche rendu visible.
+    </div>
 </asp:Content>
--- Fuite/FuiteAdmin/History.aspx.cs
+++ Fuite/FuiteAdmin/History.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
 
 namespace FuiteAdmin
 {
@@ -11,7 +13,67 @@ namespace FuiteAdmin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                return;
+            this.Update();
+        }
 
+        private void Update(ReportService.State state = ReportService.State.New)
+        {
+            string ticket = (string)Session["Ticket"];
+            ReportService.ReportServiceClient client = new ReportService.ReportServiceClient();
+            ReportService.ResultReports results = client.GetReports(ticket, state, -1, Int32.MaxValue);
+
+            if (results.Code != 0)
+                throw new HttpException(500, results.Message);
+
+            this.history.Controls.Clear();
+            string[] states = new string[]
+            {
+                    "Nouveau", "Affecté", "Traité"
+            };
+
+            results.Data = results.Data.OrderBy(x => x.Date).ToArray();
+            if (state != ReportService.State.New)
+                results.Data = results.Data.Reverse().ToArray();
+
+            foreach (ReportService.ReportContract report in results.Data)
+            {
+                HtmlTableRow tr = new HtmlTableRow();
+                HtmlTableCell td = new HtmlTableCell();
+                td.InnerText = "#"+report.Id.ToString();
+                tr.Controls.Add(td);
+                td = new HtmlTableCell();
+                td.InnerText = states[(int)report.State];
+                tr.Controls.Add(td);
+                td = new HtmlTableCell();
+                td.InnerText = report.Date.ToString("dd/MM/yy hh:mm");
+                tr.Controls.Add(td);
+                td = new HtmlTableCell();
+                HtmlAnchor a = new HtmlAnchor();
+                a.InnerText = "Voir";
+                a.Attributes["class"] = "button";
+                a.HRef = "Details.aspx?id=" + report.Id;
+                td.Controls.Add(a);
+                tr.Controls.Add(td);
+                this.history.Controls.Add(tr);
+            }
+            
+        }
+
+        protected void new_Click(object sender, EventArgs e)
+        {
+            this.Update(ReportService.State.New);
+        }
+
+        protected void current_Click(object sender, EventArgs e)
+        {
+            this.Update(ReportService.State.Affected);
+        }
+
+        protected void closed_Click(object sender, EventArgs e)
+        {
+            this.Update(ReportService.State.Closed);
         }
     }
 }
