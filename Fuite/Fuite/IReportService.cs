364ba6f6df579f35565ef59b31cb7e3688077912
Administrateur <Administrateur@VS2017>
2018-06-27 07:27:08 +0200

    Progression commentaires

--- Fuite/Fuite/IReportService.cs
+++ Fuite/Fuite/IReportService.cs
@@ -32,7 +32,7 @@ namespace FuiteAPI
         Result SetReport(SetReportRequest request);
 
         /// <summary>
-        /// Récupère la liste de tous les repots de fuite dans la base de données
+        /// Récupère la liste de tous les reports de fuite dans la base de données
         /// </summary>
         /// <returns>La liste de tous les reports de fuite</returns>
         [OperationContract]
@@ -40,6 +40,11 @@ namespace FuiteAPI
         ResponseFormat = WebMessageFormat.Json)]
         ResultReports GetReports(GetReportsRequest request);
 
+        /// <summary>
+        /// Récupère la liste de toutes les photos en fonction des paramètres précisés dans la requête 
+        /// </summary>
+        /// <param name="request">Requete contenant les paramètres de recherche de photo</param>
+        /// <returns>Objet Result contenant la liste de photos</returns>
         [OperationContract]
         [WebInvoke(Method = "*", ResponseFormat = WebMessageFormat.Json)]
         ResultPictures GetPictures(GetPicturesRequest request);
@@ -53,6 +58,11 @@ namespace FuiteAPI
         ResponseFormat = WebMessageFormat.Json)]
         ResultChanges GetChanges(GetChangesRequest request);
 
+        /// <summary>
+        /// Récupère un report de fuite en fonction des 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [OperationContract]
         [WebInvoke(Method = "*",
         ResponseFormat = WebMessageFormat.Json)]
